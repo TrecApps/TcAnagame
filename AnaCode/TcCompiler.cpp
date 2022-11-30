de345935170421f9833c511bd8e23f30e59fe5a8
John Jacko <jljacko@outlook.com>
2022-11-30 11:38:58 -0600

    Began Fleshing out Compiler Pipeline

--- AnaCode/TcCompiler.cpp
+++ AnaCode/TcCompiler.cpp
@@ -2,10 +2,38 @@
 
 TcCompiler::TcCompiler(TrecActivePointer<TFileShell> file, TrecActivePointer<TEnvironment> env, TrecActivePointer<TJsonVariable> languageDetails)
 {
+	stage = 0;
+	this->file = TrecPointerKey::ActiveToTrec<>(file);
+	if (this->file->IsDirectory())
+		throw 3;
+
+	environment = TrecPointerKey::SoftFromTrec<>(TrecPointerKey::ActiveToTrec<>(env));
+	this->languageDetails = TrecPointerKey::ActiveToTrec<>(languageDetails);
 }
 
 TString TcCompiler::Init()
 {
+	if (!stage)
+	{
+		TFile reader(file, TFile::t_file_open_existing | TFile::t_file_read);
+		if (!reader.IsOpen())
+			return L"Failed to Open File!";
+		TString contents;
+		TString contentSlice;
+
+		while (reader.ReadStringLine(contentSlice, 500))
+			contents.Append(contentSlice);
+
+		text = TrecPointerKey::ConvertPointer<TVariable, TStringVariable>(
+			TrecPointerKey::GetNewSelfTrecPointerAlt<TVariable, TStringVariable>(contents));
+		stage++;
+	}
+
+	if (stage == 1)
+	{
+		// To-Do: Flesh out Language Details
+	}
+
 	return TString();
 }
 
@@ -16,9 +44,17 @@ TString TcCompiler::PerformLex()
 
 void TcCompiler::OnSourceChange(TrecPointer<TStringVariable> updatedSource)
 {
+	if (updatedSource.Get())
+	{
+		text = updatedSource;
+		PerformLex();
+	}
 }
 
 TrecPointer<TStringVariable> TcCompiler::GetSourceString()
 {
-	return TrecPointer<TStringVariable>();
+	if (!text.Get())
+		return TrecPointer<TStringVariable>();
+	return TrecPointerKey::ConvertPointer<TVariable, TStringVariable>(
+		TrecPointerKey::GetNewTrecPointerAlt<TVariable, TStringVariable>(text->GetString()));
 }
--- AnaCode/TcCompiler.h
+++ AnaCode/TcCompiler.h
@@ -63,6 +63,7 @@ protected:
 
     TDataArray<TcLex> lexList;
     TDataArray<TcCompMessage> messages;
+    TrecPointer<TJsonVariable> languageDetails;
 
     UCHAR stage;
 
