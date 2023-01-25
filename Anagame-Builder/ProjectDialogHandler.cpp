06a1a9035ee886d27820ca5b8527ee488d762f08
John Jacko <jljacko@outlook.com>
2023-01-24 18:48:18 -0600

    Set Up Project Dialog Handler

---
+++ Anagame-Builder/ProjectDialogHandler.cpp
@@ -0,0 +1,222 @@
+#include "ProjectDialogHandler.h"
+#include <TContainerVariable.h>
+
+TString onDirectorySelect(L"OnDirectorySelect");
+TString onOkay(L"OnOkay");
+TString onCancel(L"OnCancel");
+
+TString onUpdateName(L"OnUpdateName");
+TString onSelectRecent(L"OnSelectRecent");
+TString onSelectNew(L"OnSelectNew");
+
+
+ProjectDialogHandler::ProjectDialogHandler()
+{
+    calls.push_back(&ProjectDialogHandler::OnDirectorySelect);
+    events.addEntry(onDirectorySelect, 0);
+
+    calls.push_back(&ProjectDialogHandler::OnOkay);
+    events.addEntry(onOkay, 1);
+
+    calls.push_back(&ProjectDialogHandler::OnCancel);
+    events.addEntry(onCancel, 2);
+
+    calls.push_back(&ProjectDialogHandler::OnUpdateName);
+    events.addEntry(onUpdateName, 3);
+
+    calls.push_back(&ProjectDialogHandler::OnSelectNew);
+    events.addEntry(onSelectNew, 4);
+
+    calls.push_back(&ProjectDialogHandler::OnSelectRecent);
+    events.addEntry(onSelectRecent, 5);
+}
+
+void ProjectDialogHandler::Initialize(TrecPointer<TPage> page)
+{
+    dialogWindow = TrecPointerKey::ConvertPointer<DrawingBoard, TDialog>(page->GetDrawingBoard());
+
+    TrecPointer<AnafacePage> aPage = TrecPointerKey::ConvertPointer<TPage, AnafacePage>(page);
+
+    okayButton = aPage->GetControlById(L"OkButton");
+    inputDirectory = TrecPointerKey::ConvertPointer<TControl, TTextInput>(aPage->GetControlById(L"DirectoryField"));
+    inputName = TrecPointerKey::ConvertPointer<TControl, TTextInput>(aPage->GetControlById(L"NameField"));
+
+    auto instance = TInstance::GetInstance();
+
+    existingProjects = TrecPointerKey::ConvertPointer<TVariable, TArrayVariable>(instance->GetExisitngProjects());
+    availableProjects = TrecPointerKey::ConvertPointer<TVariable, TArrayVariable>(instance->GetAvailableProjectTypes());
+}
+
+bool ProjectDialogHandler::OnChar(bool fromChar, UINT nChar, UINT nRepCnt, UINT nFlags, message_output* mOut)
+{
+    return false;
+}
+
+void ProjectDialogHandler::SetSelf(TrecPointer<TPage::EventHandler> handleSelf)
+{
+}
+
+TrecPointer<TPage> ProjectDialogHandler::GetPage()
+{
+    return TrecPointer<TPage>();
+}
+
+void ProjectDialogHandler::HandleEvents(TDataArray<TPage::EventID_Cred>& eventAr)
+{
+    int e_id = -1;
+    EventArgs ea;
+    for (UINT c = 0; c < eventAr.Size(); c++)
+    {
+        auto tc = eventAr.at(c).args;
+
+        if (!tc.Get()) continue;
+
+        UINT u_id = 0;
+        if (!events.retrieveEntry(tc->methodID, u_id))
+            continue;
+        e_id = u_id;
+        // At this point, call the appropriate method
+        if (e_id > -1 && e_id < calls.Size())
+        {
+            // call method
+            if (calls[e_id])
+                (this->*calls[e_id])(eventAr[c].control, ea);
+        }
+        eventAr[c].args.Nullify();
+    }
+}
+
+void ProjectDialogHandler::Close()
+{
+    inputDirectory.Nullify();
+    inputName.Nullify();
+    auto win = dialogWindow;
+    dialogWindow.Nullify();
+    if (win.Get())
+        win->Close();
+}
+
+void ProjectDialogHandler::AssessOkay()
+{
+    bool works = true;
+    if (!projectData.builderName.GetTrim().GetSize())
+        works = false;
+    if (works && !projectData.environmentName.GetTrim().GetSize())
+        works = false;
+    if (works && !projectData.projectName.GetTrim().GetSize())
+        works = false;
+    if (works && !projectData.directory.Get())
+        works = false;
+
+    okayButton->setActive(works);
+}
+
+void ProjectDialogHandler::OnDirectorySelect(TrecPointer<TPage> tc, EventArgs ea)
+{
+    TString error;
+    TrecPointer<TFileShell> targetDirectory = OpenDirectoryDialog(TrecPointer<TDirectory>(), error);
+    TrecPointer<TDirectory> actDirectory = TrecPointerKey::ConvertPointer<TFileShell, TDirectory>(targetDirectory);
+    if (actDirectory.Get())
+        this->projectData.directory = actDirectory;
+    AssessOkay();
+}
+
+void ProjectDialogHandler::OnCancel(TrecPointer<TPage> tc, EventArgs ea)
+{
+    Close();
+}
+
+void ProjectDialogHandler::OnOkay(TrecPointer<TPage> tc, EventArgs ea)
+{
+    if (dialogWindow.Get())
+    {
+        TrecPointer<TVariable> proj = TrecPointerKey::GetNewSelfTrecPointerAlt<TVariable, TJsonVariable>();
+        TrecPointer<TJsonVariable> jProj = TrecPointerKey::ConvertPointer<TVariable, TJsonVariable>(proj);
+
+        jProj->SetField(L"builderType", TrecPointerKey::GetNewSelfTrecPointerAlt<TVariable, TStringVariable>(projectData.builderName));
+        jProj->SetField(L"projectType", TrecPointerKey::GetNewSelfTrecPointerAlt<TVariable, TStringVariable>(projectData.environmentName));
+        jProj->SetField(L"name", TrecPointerKey::GetNewSelfTrecPointerAlt<TVariable, TStringVariable>(projectData.projectName));
+        jProj->SetField(L"directory", TrecPointerKey::GetNewSelfTrecPointerAlt<TVariable, TStringVariable>(projectData.directory->GetPath()));
+
+
+        dialogWindow->SetResult(proj);
+    }
+    Close();
+}
+
+void ProjectDialogHandler::OnSelectNew(TrecPointer<TPage> tc, EventArgs ea)
+{
+    UINT index = static_cast<UINT>(ea.arrayLabel);
+
+    TrecPointer<TVariable> proj;
+    if (this->availableProjects.Get() && availableProjects->At(proj, index))
+    {
+        auto jProj = TrecPointerKey::ConvertPointer<TVariable, TJsonVariable>(proj);
+        assert(jProj.Get());
+
+        if (jProj->RetrieveField(L"builderType", proj))
+        {
+            auto strVar = TrecPointerKey::ConvertPointer<TVariable, TStringVariable>(proj);
+            assert(strVar.Get());
+            projectData.builderName.Set(strVar->GetString());
+        }
+
+        if (jProj->RetrieveField(L"projectType", proj))
+        {
+            auto strVar = TrecPointerKey::ConvertPointer<TVariable, TStringVariable>(proj);
+            assert(strVar.Get());
+            projectData.environmentName.Set(strVar->GetString());
+        }
+
+    }
+    AssessOkay();
+}
+
+void ProjectDialogHandler::OnSelectRecent(TrecPointer<TPage> tc, EventArgs ea)
+{
+    UINT index = static_cast<UINT>(ea.arrayLabel);
+
+    TrecPointer<TVariable> proj;
+    if (this->existingProjects.Get() && existingProjects->At(proj, index))
+    {
+        auto jProj = TrecPointerKey::ConvertPointer<TVariable, TJsonVariable>(proj);
+        assert(jProj.Get());
+
+        if (jProj->RetrieveField(L"builderType", proj))
+        {
+            auto strVar = TrecPointerKey::ConvertPointer<TVariable, TStringVariable>(proj);
+            assert(strVar.Get());
+            projectData.builderName.Set(strVar->GetString());
+        }
+
+        if (jProj->RetrieveField(L"projectType", proj))
+        {
+            auto strVar = TrecPointerKey::ConvertPointer<TVariable, TStringVariable>(proj);
+            assert(strVar.Get());
+            projectData.environmentName.Set(strVar->GetString());
+        }
+
+        if (jProj->RetrieveField(L"name", proj))
+        {
+            auto strVar = TrecPointerKey::ConvertPointer<TVariable, TStringVariable>(proj);
+            assert(strVar.Get());
+            projectData.projectName.Set(strVar->GetString());
+            inputName->SetText(projectData.projectName);
+        }
+
+        if (jProj->RetrieveField(L"directory", proj))
+        {
+            auto strVar = TrecPointerKey::ConvertPointer<TVariable, TStringVariable>(proj);
+            assert(strVar.Get());
+            projectData.directory = TrecPointerKey::ConvertPointer<TFileShell,TDirectory>(TFileShell::GetFileInfo(strVar->GetString()));
+            if (projectData.directory.Get())
+                inputDirectory->SetText(projectData.directory->GetPath());
+        }
+    }
+    AssessOkay();
+}
+
+void ProjectDialogHandler::OnUpdateName(TrecPointer<TPage> tc, EventArgs ea)
+{
+    projectData.projectName.Set(ea.text);
+}
---
+++ Anagame-Builder/ProjectDialogHandler.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <TPage.h>
+#include <TDialog.h>
+#include <AnafacePage.h>
+#include <TTextInput.h>
+#include <TInstance.h>
+class ProjectDialogHandler :
+    public TPage::EventHandler
+{
+	typedef void (ProjectDialogHandler::* ProjectCall)(TrecPointer<TPage> tc, EventArgs ea);
+	TDataArray<ProjectCall> calls;
+	TDataMap<UINT> events;
+	TrecPointer<TDialog> dialogWindow;
+
+	TrecPointer<TTextInput> inputDirectory;
+	TrecPointer<TTextInput> inputName;
+	TrecPointer<TControl> okayButton;
+
+	TProjectData projectData;
+
+	TrecPointer<TArrayVariable> existingProjects;
+	TrecPointer<TArrayVariable> availableProjects;
+	
+public:
+	ProjectDialogHandler();
+
+	/**
+	 * Method: EventHandler::Initialize
+	 * Purpose: Initializes the Handler so that it has direct Access to certain Controls held by the page
+	 * Parameters: TrecPointer<Page> page - page that holds the Controls to latch on to
+	 * Returns: void
+	 *
+	 * Attributes: abstract
+	 */
+	virtual void Initialize(TrecPointer<TPage> page) override;
+
+	/**
+	 * Method: EventHandler::OnChar
+	 * Purpose: Handles Character input from the Instance of Anagame
+	 * Parameters: bool fromChar - true if it came form On_Char from windows, false if it came from a key_down
+	 *				UINT nChar - the character submitted
+	 *				UINT nRepCnt - number od times to add the character
+	 *				UINT nFlags - unused
+	 *				message_output* mOut - result
+	 * Returns: bool
+	 */
+	virtual bool OnChar(bool fromChar, UINT nChar, UINT nRepCnt, UINT nFlags, message_output* mOut) override;
+
+	virtual void SetSelf(TrecPointer<TPage::EventHandler> handleSelf) override;
+
+	virtual TrecPointer<TPage> GetPage() override;
+
+	virtual void HandleEvents(TDataArray<TPage::EventID_Cred>& eventAr) override;
+
+private:
+	void Close();
+	void AssessOkay();
+
+	void OnDirectorySelect(TrecPointer<TPage> tc, EventArgs ea);
+	void OnCancel(TrecPointer<TPage> tc, EventArgs ea);
+	void OnOkay(TrecPointer<TPage> tc, EventArgs ea);
+	void OnSelectNew(TrecPointer<TPage> tc, EventArgs ea);
+	void OnSelectRecent(TrecPointer<TPage> tc, EventArgs ea);
+	void OnUpdateName(TrecPointer<TPage> tc, EventArgs ea);
+};
+
